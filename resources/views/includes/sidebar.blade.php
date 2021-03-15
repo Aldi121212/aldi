8bf9ebd9f25d3959a19131ad6e9ccf80ff474cd6
aldi121212 <aldiazharimaulanayusuf@gmail.com>
2021-03-15 10:14:26 +0700

    first commit aldi

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="row" id="body-row">
+    <div id="sidebar-container" class="sidebar-expanded d-none d-md-block">
+        <ul class="list-group">
+            <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
+                <small>MENU</small>
+            </li>
+            <a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="bg-success list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-start align-items-center">
+                    <span class="fa fa-dashboard fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Profile</span>
+                    <span class="submenu-icon ml-auto"></span>
+                </div>
+            </a>
+            <div id="submenu1" class="collapse sidebar-submenu">
+                <a href="#" class="list-group-item list-group-item-action bg-success text-white">
+                    <span class="menu-collapsed">home</span>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action bg-success text-white">
+                    <span class="menu-collapsed">about</span>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action bg-success text-white">
+                    <span class="menu-collapsed">Contak</span>
+                </a>
+            </div>
+            
+        </ul>
+    </div>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+@section('title', 'Halaman Home')
+@section('content')
+    <br>
+<div class="jumbotron bg-success">
+    <div style="font-family: arial;" class="text-center">
+      <h1 class="display-4 text-light" style=" font-family: arial; font-weight: bold;">REVTIL BERKAKI</h1>
+      <h5 class="display-5" style="font-family: arial; font-weight: bold; margin: 0px;">REVTIL LIAR</h5>
+    </div>
+  </div>
+<div style="font-family: arial;" class="container mt-3 pt-2">
+    <center>
+    
+</center>
+<br><br>
+<div class="card text-center">
+  <div class="card-header">
+   Jenis-jenis
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Revtil berkaki 4 </h5>
+    <p class="card-text">Habitat di air atau di darat.</p>
+    <a href="#" class="btn btn-success">berikut list revtil</a>
+  </div>
+  <div class="card-footer text-muted">
+    REVTIL
+  </div>
+</div>
+    <div class="row" style="margin-top: 20px;">
+        <div class="col-md-12">
+            <main role="main">
+                <div style="font-family: arial;" class="container">
+                  <div class="row">
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h5 style="font-family: arial; font-weight: bold;" class="card-title">DRAGON KOMODO</h5>
+                              <p class="card-text">Habitat, Makanan, karakter, usia,</p>
+                              <a href="#" class="btn btn-success">Go Klik Here</a>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h5 style="font-family: arial; font-weight: bold;" class="card-title">iguana</h5>
+                              <p class="card-text">Habitat, Makanan, karakter, usia,</p>
+                              <a href="#" class="btn btn-success">Go Klik Here</a>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h5 style="font-family: arial; font-weight: bold;" class="card-title">Kadal Lidah Biru</h5>
+                              <p class="card-text">Habitat, Makanan, karakter, usia,</p>
+                              <a href="#" class="btn btn-success">Go Klik Here</a>
+                            </div>
+                          </div>
+                    </div>
+                  </div>
+                  <br>
+                   <div class="row">
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h5 style="font-family: arial; font-weight: bold;" class="card-title">Kadal Mata Merah</h5>
+                              <p class="card-text">Habitat, Makanan, karakter, usia,</p>
+                              <a href="#" class="btn btn-success">Go Klik Here</a>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h5 style="font-family: arial; font-weight: bold;" class="card-title">Biawak Air</h5>
+                              <p class="card-text">Habitat, Makanan, karakter, usia,</p>
+                              <a href="#" class="btn btn-success">Go Klik Here</a>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h5 style="font-family: arial; font-weight: bold;" class="card-title">Leopart Gecko</h5>
+                              <p class="card-text">Habitat, Makanan, karakter, usia,</p>
+                              <a href="#" class="btn btn-success">Go Klik Here</a>
+                            </div>
+                          </div>
+                    </div>
+                  </div>
+                  <hr>
+                </div>
+              </main>
+
+@endsection
+
+@section('js')
+    <script>
+        console.log('Helo')
+    </script>
+@endsection
